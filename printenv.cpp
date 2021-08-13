82c41bab5d2bf76400f1c4b6f52a71e51fd3545b
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2021-08-14 02:31:04 +0800

    first program

---
+++ printenv.cpp
@@ -0,0 +1,48 @@
+﻿#include<vector>
+#include<string>
+#include<iostream>
+#include<windows.h>
+ 
+void write(const std::wstring& str);
+std::vector<std::wstring> getEnvs();
+
+int main()
+{
+	for (auto str : getEnvs())
+	{
+		str += L"\n";
+		write(str);
+	}
+	return 0;
+}
+
+
+void write(const std::wstring& str) {
+	WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), str.c_str(), str.size(), NULL, nullptr);
+}
+
+std::vector<std::wstring> getEnvs() {
+	auto envStr = GetEnvironmentStringsW();
+	if (envStr == nullptr)
+		return {};
+	auto pt = envStr;
+
+	std::vector<std::wstring> envs;
+	std::wstring buffStr;
+
+	while (true)
+	{
+		if (*pt == 0)
+		{
+			envs.push_back(std::move(buffStr));
+			if (*(pt + 1) == 0)
+				break;
+		}
+		else {
+			buffStr.push_back(*pt);
+		}
+		pt++;
+	}
+	FreeEnvironmentStringsW(envStr);
+	return envs;
+}
